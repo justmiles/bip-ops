2769af11c0c6a7d29aba9a2c2734cc95f38b22cc
justmiles <j.milesmaddox@gmail.com>
2025-11-20 12:25:49 -0600

    improve game update process

--- s6-overlay/s6-rc.d/update-game/scripts/update-game.sh
+++ s6-overlay/s6-rc.d/update-game/scripts/update-game.sh
@@ -25,11 +25,33 @@ CURRENT_OWNER=$(stat -c "%U" /game)
 
 cd /game
 
-USER=bipops HOME=/home/bipops s6-setuidgid bipops /usr/lib/steamcmd/steamcmd.sh \
-  +@sSteamCmdForcePlatformType ${PLATFORM_TYPE} \
-  +force_install_dir /game \
-  +login ${LOGIN} \
-  +app_update ${APP_UPDATE} \
-  +quit
+# install the latest
+function updateServer {
+  USER=bipops HOME=/home/bipops s6-setuidgid bipops /usr/lib/steamcmd/steamcmd.sh \
+    +@sSteamCmdForcePlatformType ${PLATFORM_TYPE} \
+    +force_install_dir /game \
+    +login ${LOGIN} \
+    +app_update ${APP_UPDATE} \
+    +quit
+}
 
-echo "Game up to date!"
+# Check for updates
+if [ -f steamapps/appmanifest_$GAME_ID.acf ]; then
+  CURRENT_VERSION=$(grep '"buildid"' steamapps/appmanifest_$GAME_ID.acf | awk '{print $2}' | tr -d '"')
+  LATEST_VERSION=$(/usr/lib/steamcmd/steamcmd.sh +login anonymous +app_info_print $GAME_ID +quit \
+    | grep -EA 1000 '^\s+"branches"' \
+    | grep -EA 5 '^\s+"public"' \
+    | grep -m 1 -E '^\s+"buildid"' \
+    | awk '{print $2}' | tr -d '"')
+
+  if [[ "$CURRENT_VERSION" != "$LATEST_VERSION" ]]; then
+    echo "Update available: $CURRENT_VERSION -> $LATEST_VERSION"
+    rm -f steamapps/appmanifest_$GAME_ID.acf
+    updateServer
+  else
+    echo "No update available. Current version: $CURRENT_VERSION"
+  fi
+else
+  # no steamapps manifest found, let's install the game
+  updateServer
+fi
